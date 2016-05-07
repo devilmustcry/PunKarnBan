7e5c13e39226995ba43e5ccd49cde92d91d13f8d
devilmustcry <eaglization@gmail.com>
2016-05-07 19:58:16 +0700

    Add tap photo

--- app/src/main/java/com/sandstorm/softspec/punkarnban/Activities/MainActivity.java
+++ app/src/main/java/com/sandstorm/softspec/punkarnban/Activities/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sandstorm.softspec.punkarnban.R;
 
@@ -18,13 +20,40 @@ import com.sandstorm.softspec.punkarnban.R;
 public class MainActivity extends Activity {
 
 
+    private ImageView tap;
+
+    private TextView valText;
+
+    int val = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
+        initComponents();
 
+    }
+
+    private void initComponents() {
+
+        tap = (ImageView) findViewById(R.id.tap);
+        valText = (TextView) findViewById(R.id.test_text);
+
+        tap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(val%2==0){
+                    tap.setImageResource(R.drawable.tap_tapping);
+
+                }
+                else
+                    tap.setImageResource(R.drawable.tap_default);
+
+                valText.setText(val++ + "");
+            }
+        });
 
     }
 
