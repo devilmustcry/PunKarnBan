980ca6adc393413fe517c7b63c08e455d97a4ab9
Zen Pattiyathanee <Zen@Zens-MacBook-Air.local>
2016-05-07 17:41:35 +0700

    modified stationery1

--- app/src/main/java/com/sandstorm/softspec/punkarnban/Models/Weapon/Stationery.java
+++ app/src/main/java/com/sandstorm/softspec/punkarnban/Models/Weapon/Stationery.java
@@ -1,5 +1,8 @@
 package com.sandstorm.softspec.punkarnban.Models.Weapon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by FTTX on 5/7/2016 AD.
  */
@@ -7,12 +10,15 @@ public class Stationery {
     private static Stationery instance;
     private int level;
     private long wpt;
-    private String name;
+    private long price;
+    private final String [] names  = {"Rusty pencil", "Standard pencil" , "Silver pencil" , "Platinum pencil" , "Chairman pencil",
+            "Embedded Computer pencil" , "Quantum Computer pencil" , "Heavenly Being pencil" ,
+            "The greatest ever made pencil" , "Pen"};
 
     private Stationery(){
         level = 1;
         wpt = 1;
-        name = "Standard pencil";
+        price = 5;
     }
 
     public static Stationery getInstance(){
@@ -23,30 +29,31 @@ public class Stationery {
     }
 
     public int getLevel(){
-        return  level;
+        return level;
+    }
+
+    public long getPrice(){
+        return price;
     }
 
     public long getWPT(){
         return wpt;
     }
 
-    public void setLevel(int new_level){
-        level = new_level;
+    public void levelUp(){
+        level = level++;
+        price = (int)Math.floor(price * 1.02);
     }
 
-    public void setName(String new_name){
-        name = new_name;
-    }
-
-    public void setWPT(long new_wpt){
-        wpt = new_wpt;
-    }
 
     public String getFullName(){
-        return name + " lv. " + level;
+        return getName(level) + " lv." + level;
     }
 
+    private String getName(int level){
+        return names[((int) Math.floor(level / 100.0))];
 
+    }
 
 
 }
